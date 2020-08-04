d7a6c77d180d97f8d777d8f414d856f559d9ca37
IestynGage <33298011+IestynGage@users.noreply.github.com>
2020-08-05 00:22:00 +0100

    Add files via upload

---
+++ course-api-data/src/main/java/com/iestyn/courseapidata/CourseRepository.java
@@ -0,0 +1,7 @@
+package com.iestyn.courseapidata;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CourseRepository extends CrudRepository<Course, String>{
+
+}
---
+++ course-api-data/src/main/java/com/iestyn/topic/TopicRepository.java
@@ -0,0 +1,7 @@
+package com.iestyn.topic;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface TopicRepository extends CrudRepository<Topic, String>{
+
+}
